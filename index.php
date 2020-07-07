9adb96674ad608ee4b49ed585360c46aa9677c6c
kalle3e <64237549+kalle3e@users.noreply.github.com>
2020-07-07 18:41:54 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+include 'includes/libraries.php';
+
+$fline = file('Files/instructionsData.txt');
+
+$store = new processXyf();
+$news = [NORTH, SOUTH, EAST, WEST];
+
+for ($i=0; $i < count($fline); $i++)
+{
+    $line = explode(' ', $fline[$i]);
+    if (1 < count($line) && PLACE !== $line[0])
+    {
+        errorDisplay();
+    }
+    if (1 < count($line))  // checking PLACE is good
+    {
+        $lineP2 = explode(',', $line[1]); //  for checking the 'PLACE' line
+        if ( is_numeric($lineP2[0]) && is_numeric($lineP2[1]) && in_array(rtrim($lineP2[2]),$news) )
+        {
+            if ( !( ( PLACE == rtrim($line[0]) ) && 3 == count($lineP2) ) )
+            {errorDisplay();}
+        }
+        else    // PLACE not in correct format
+        {
+            errorDisplay();
+        }
+        $store->storeXyfPlace($line[1]);
+    }
+    else  // not 'PLACE' line
+    {
+        /**
+         *  MOVE LEFT RIGHT REPORT
+         */
+        switch (rtrim($fline[$i]))
+        {
+            case MOVE:
+                $store->updateXy();
+                break;
+            case LEFT:
+                $store->leftRight = LEFT;
+                $store->getFacingDirectionNew();
+                break;
+            case RIGHT:
+                $store->leftRight = RIGHT;
+                $store->getFacingDirectionNew();
+                break;
+            case REPORT:
+                $store->reportNewCoords();
+                if ($i == count($fline))
+                {exit;}
+                else
+                {break;}
+            default:
+                errorDisplay();
+        }
+    }
+}
+?>
